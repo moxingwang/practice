e4adbd1fa6fdd374c7f566dca09a2d48d9d072ca
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-08-28 19:31:54 +0800

    commit

--- dubbo/dubbo-consumer/src/main/java/com/mo/dubbo/consumer/DubboConsumerApplication.java
+++ dubbo/dubbo-consumer/src/main/java/com/mo/dubbo/consumer/DubboConsumerApplication.java
@@ -2,11 +2,13 @@ package com.mo.dubbo.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath:spring/applicationContext-*.xml"})
 public class DubboConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DubboConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerApplication.class, args);
+    }
 }
