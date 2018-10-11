f90ac428684ab56eb3e3632469582bbba4a7856b
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-10-11 10:01:07 +0800

    commit

---
+++ mq/spring-xml/src/main/java/top/moxingwang/springxml/SpringXmlApplication.java
@@ -0,0 +1,12 @@
+package top.moxingwang.springxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringXmlApplication.class, args);
+	}
+}
