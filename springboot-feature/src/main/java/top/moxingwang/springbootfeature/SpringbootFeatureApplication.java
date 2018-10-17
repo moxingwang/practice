c8f09c5e0ab4619675be0a71c02ea365112e4a5f
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-10-17 16:43:08 +0800

    commit

---
+++ springboot-feature/src/main/java/top/moxingwang/springbootfeature/SpringbootFeatureApplication.java
@@ -0,0 +1,12 @@
+package top.moxingwang.springbootfeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFeatureApplication.class, args);
+	}
+}
