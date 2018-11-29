9056e758b3dfdf06ada13a9c2e78804dd7af68fc
m65536 <$mx@github#159>
2018-11-29 19:57:50 +0800

    commit

--- search/javaclient/src/test/java/top/moxingwang/elasticsearch/TransportClientApplicationTest.java
+++ search/javaclient/src/test/java/top/moxingwang/elasticsearch/TransportClientApplicationTest.java
@@ -2,9 +2,7 @@ package top.moxingwang.elasticsearch;
 
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +13,9 @@ import java.net.UnknownHostException;
 //import org.elasticsearch.common.transport.InetSocketTransportAddress;
 //import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
+/**
+ * https://www.elastic.co/guide/en/elasticsearch/client/java-api/2.4/java-docs.html
+ */
 public class TransportClientApplicationTest {
     private TransportClient client;
 
@@ -22,19 +23,11 @@ public class TransportClientApplicationTest {
 
     @Before
     public void elasticsearchRestHighLevelClient() throws UnknownHostException {
-        Settings settings = Settings.builder()
-                .put("cluster.name", "elasticsearch").build();
-        client = new PreBuiltTransportClient(settings)
+
+
+        TransportClient client = TransportClient.builder().build()
                 .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("esuc.dev.rs.com"), 9300)
-//                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("esuc.dev.rs.com"), 9300)
                 );
-
-// on shutdown
-
-
-//        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
-//                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("esuc.dev.rs.com"), 9300));
-//        System.out.println(1);
         GetResponse response = client.prepareGet("trade-order-orders2", "data", "AWdeq7vLqHbFY58w-GxL").get();
 
         System.out.println(1);
