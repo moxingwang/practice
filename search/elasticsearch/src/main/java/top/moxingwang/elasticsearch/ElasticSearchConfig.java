5cd58a26b2a7bb206c5aca0e34e38e5006b37565
m65536 <$mx@github#159>
2018-11-29 10:51:17 +0800

    commit

--- search/elasticsearch/src/main/java/top/moxingwang/elasticsearch/ElasticSearchConfig.java
+++ search/elasticsearch/src/main/java/top/moxingwang/elasticsearch/ElasticSearchConfig.java
@@ -6,24 +6,37 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
 /**
  * @description:
  * @author: MoXingwang 2018-11-28 20:21
  **/
 @Configuration
 public class ElasticSearchConfig {
+    private RestHighLevelClient client;
+
     @Bean
     public RestHighLevelClient elasticsearchRestHighLevelClient() {
-        RestHighLevelClient client = new RestHighLevelClient(
+        client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("esuc.dev.rs.com", 9200, "http")));
+                        new HttpHost("esuc.dev.rs.com", 9200, "http")
+                ));
 
         return client;
     }
 
+    @PreDestroy
+    public void destroy() {
+        try {
+            if (null != client) {
+                client.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    /**
-     * todo client.close();
-     */
 
 }
--- search/elasticsearch/src/test/java/top/moxingwang/elasticsearch/ElasticsearchApplicationTests.java
+++ search/elasticsearch/src/test/java/top/moxingwang/elasticsearch/ElasticsearchApplicationTests.java
@@ -1,6 +1,5 @@
 package top.moxingwang.elasticsearch;
 
-import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -23,62 +22,66 @@ import java.io.IOException;
 @SpringBootTest
 public class ElasticsearchApplicationTests {
 
-	@Autowired
-	private ElasticsearchTemplate elasticsearchTemplate;
-	@Autowired
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+    @Autowired
     private RestHighLevelClient restHighLevelClient;
 
-	@Document(indexName = "test", type = "test")
-	class User{
-			private String user;
-			private String id;
 
-		public User(String user, String id) {
-			this.user = user;
-			this.id = id;
-		}
-
-		public String getUser() {
-			return user;
-		}
-
-		public void setUser(String user) {
-			this.user = user;
-		}
-
-		public String getId() {
-			return id;
-		}
-
-		public void setId(String id) {
-			this.id = id;
-		}
-	}
+    /**
+     * -----------  rest client 方式访问    -------------
+     */
 
     @Test
     public void query() throws IOException {
-		SearchRequest searchRequest = new SearchRequest("trade-order-order");
-		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-//		sourceBuilder.query(QueryBuilders.matchQuery("des", "软件"));
-		sourceBuilder.from(0);
-		sourceBuilder.size(5);
-		searchRequest.source(sourceBuilder);
+        SearchRequest searchRequest = new SearchRequest("trade-order-order");
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.matchQuery("name", ""));
+        sourceBuilder.from(0);
+        sourceBuilder.size(5);
+        searchRequest.source(sourceBuilder);
 
-		SearchResponse response = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+        SearchResponse response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
 
-		System.out.println(response.getHits());
-		System.out.println(1);
-
-
+        System.out.println(response.getHits());
+        System.out.println(1);
     }
 
 
+    /**
+     * -----------  spring boot java client 方式访问    -------------
+     */
     @Test
-	public void contextLoads() {
-//		IndexQuery indexQuery = new IndexQueryBuilder().withObject(new User("111","111")).build();
-//		elasticsearchTemplate.index(indexQuery);
-//        restHighLevelClient.
+    public void contextLoads() {
+        IndexQuery indexQuery = new IndexQueryBuilder().withObject(new User("111", "111")).build();
+        elasticsearchTemplate.index(indexQuery);
 
-	}
+    }
 
+    @Document(indexName = "test", type = "test")
+    class User {
+        private String user;
+        private String id;
+
+        public User(String user, String id) {
+            this.user = user;
+            this.id = id;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
 }
