d274db2637d979586c35eb1ee6c2225d7132374d
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-12-25 18:46:17 +0800

    v

--- test/common-demo/src/main/java/top/moxingwang/demo/arithmetic/SortTest.java
+++ test/common-demo/src/main/java/top/moxingwang/demo/arithmetic/SortTest.java
@@ -5,5 +5,7 @@ package top.moxingwang.demo.arithmetic;
  * @author: MoXingwang 2018-12-25 16:00
  **/
 public class SortTest {
-    
+    public void quickSort(int[] source, int left, int right) {
+
+    }
 }
