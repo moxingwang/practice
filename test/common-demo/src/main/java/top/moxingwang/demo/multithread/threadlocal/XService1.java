8fefaa8cb2c6579c055b2670718dee4b74552cff
m65536 <$mx@github#159>
2019-01-02 23:24:44 +0800

    commit

--- test/common-demo/src/main/java/top/moxingwang/demo/multithread/threadlocal/XService1.java
+++ test/common-demo/src/main/java/top/moxingwang/demo/multithread/threadlocal/XService1.java
@@ -1,31 +1,13 @@
 package top.moxingwang.demo.multithread.threadlocal;
 
-import org.junit.Test;
-
 /**
  * @author MoXingwang on 2018/1/23.
  */
 public class XService1 {
     ThreadLocal<String> threadLocal1 = new ThreadLocal<String>();
 
-    static {
-        System.out.println("XService1");
-    }
-
-
-    @Test
     public void testThreadLocalValue() {
         threadLocal1.set("test1");
         String str1 = threadLocal1.get();
-
-        Thread thread2 = new Thread(() -> {
-            threadLocal1.set("test2");
-            String str2 = threadLocal1.get();
-        });
-
-        thread2.start();
     }
-
-
-
 }
--- test/common-demo/src/main/java/top/moxingwang/demo/multithread/threadlocal/XService2.java
+++ test/common-demo/src/main/java/top/moxingwang/demo/multithread/threadlocal/XService2.java
@@ -8,22 +8,10 @@ import org.junit.Test;
 public class XService2 {
     ThreadLocal<String> threadLocal2 = new ThreadLocal<String>();
 
-
-    static {
-        System.out.println("XService2");
-    }
-
     @Test
     public void testThreadLocalValue() {
-        threadLocal2.set("test1");
-        String str1 = threadLocal2.get();
-
-        Thread thread2 = new Thread(() -> {
-            threadLocal2.set("test2");
-            String str2 = threadLocal2.get();
-        });
-
-        thread2.start();
+        threadLocal2.set("test2");
+        String str2 = threadLocal2.get();
     }
 
 }
