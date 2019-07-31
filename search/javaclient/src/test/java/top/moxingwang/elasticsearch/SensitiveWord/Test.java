814654686de9103897e12980cf06d87bc5d1429d
moxingwang <xingwang.mo@chinaredstar.com>
2019-07-31 10:08:42 +0800

    11

--- search/javaclient/src/test/java/top/moxingwang/elasticsearch/SensitiveWord/Test.java
+++ search/javaclient/src/test/java/top/moxingwang/elasticsearch/SensitiveWord/Test.java
@@ -10,6 +10,7 @@ import java.util.Set;
  * @author: MoXingwang 2019-07-29 13:10
  **/
 public class Test {
+    //https://www.cnblogs.com/AlanLee/p/5329555.html
     public static void main(String[] args) {
 
         // 初始化敏感词库对象
@@ -17,9 +18,12 @@ public class Test {
         // 从数据库中获取敏感词对象集合（调用的方法来自Dao层，此方法是service层的实现类）
         List<SensitiveWord> sensitiveWords = new ArrayList<>();
         {
-            sensitiveWords.add(new SensitiveWord("我"));
-            sensitiveWords.add(new SensitiveWord("我的"));
-            sensitiveWords.add(new SensitiveWord("我哈哈"));
+            sensitiveWords.add(new SensitiveWord("我们的空时间哈哈哈哈哈"));
+//            sensitiveWords.add(new SensitiveWord("我的"));
+//            sensitiveWords.add(new SensitiveWord("我哈哈"));
+//            sensitiveWords.add(new SensitiveWord("你"));
+//            sensitiveWords.add(new SensitiveWord("你的"));
+//            sensitiveWords.add(new SensitiveWord("他们的"));
         }
 
         // 构建敏感词库
