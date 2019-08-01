dc404c73a775864dfa458954d68c5bdec066c0f3
moxingwang <xingwang.mo@chinaredstar.com>
2019-08-01 14:47:47 +0800

    11

--- springboot-feature/src/main/java/top/moxingwang/springbootfeature/jpa/OrderRepository.java
+++ springboot-feature/src/main/java/top/moxingwang/springbootfeature/jpa/OrderRepository.java
@@ -2,17 +2,19 @@ package top.moxingwang.springbootfeature.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import top.moxingwang.springbootfeature.model.TxOrderEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
  * @description:
  * @author: MoXingwang 2019-08-01 14:05
  **/
-public interface  OrderRepository extends JpaRepository<TxOrderEntity, Long> {
+public interface OrderRepository extends JpaRepository<TxOrderEntity, Long> {
     Optional<TxOrderEntity> findById(Long id);
 
+    @Query(value = "select * from tx_order  where order_type = ?1",nativeQuery = true)
+    List<TxOrderEntity> listOrder(int orderType);
 
 }
--- springboot-feature/src/test/java/top/moxingwang/springbootfeature/SpringbootFeatureApplicationTests.java
+++ springboot-feature/src/test/java/top/moxingwang/springbootfeature/SpringbootFeatureApplicationTests.java
@@ -9,27 +9,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 import top.moxingwang.springbootfeature.jpa.OrderRepository;
 import top.moxingwang.springbootfeature.model.TxOrderEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootFeatureApplicationTests {
 
-	@Autowired
-	private OrderRepository orderRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private StringEncryptor encryptor;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
-	@Autowired
-	private StringEncryptor encryptor;
+    @Test
+    public void getPass() {
 
-	@Test
-	public void getPass() {
-
-		Optional<TxOrderEntity>  s  = orderRepository.findById(1L);
-		TxOrderEntity ss = s.get();
-		System.out.println(1);
-	}
+        Optional<TxOrderEntity> s = orderRepository.findById(1L);
+        TxOrderEntity ss = s.get();
+        List<TxOrderEntity> list = orderRepository.listOrder(1);
+        System.out.println(1);
+    }
 }
