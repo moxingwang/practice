48ec9bbc70ebdf3c39cea4be0203a350053e7570
moxingwang <xingwang.mo@chinaredstar.com>
2019-12-13 16:41:16 +0800

    commit

--- test/common-demo/src/main/java/top/moxingwang/demo/classloader/testobj/MyObj2.java
+++ test/common-demo/src/main/java/top/moxingwang/demo/classloader/testobj/MyObj2.java
@@ -10,12 +10,29 @@ public class MyObj2 {
     public static int  a ;
     public static final List list = new ArrayList(100);
 
+    public static void main(String[] args) {
+        System.out.println(1);
+        MyObj2 myObj2 = new MyObj2();
+        myObj2.testAdd();
+    }
+
     static  {
         a=1;
         System.out.println("执行静态方法" + a);
     }
 
     public void getString(String a ){
+        int i = 10;
+        i++;
+        i--;
+
+        Integer integer = new Integer(i);
+
         System.out.println("执行方法getString");
+    }
+
+    public void testAdd(){
+        int i = 10;
+        System.out.println(i++);
     }
 }
