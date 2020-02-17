12e6e40b495f7e3d7e4cc8d24442b3294b5e05fe
moxingwang <xingwang.mo@chinaredstar.com>
2020-02-17 18:44:52 +0800

    commit

--- test/common-demo/src/main/java/top/moxingwang/demo/proyx/aspectj/HelloWorld.java
+++ test/common-demo/src/main/java/top/moxingwang/demo/proyx/aspectj/HelloWorld.java
@@ -1,5 +1,8 @@
 package top.moxingwang.demo.proyx.aspectj;
 
+/**
+ * -[IntelliJ IDEA 下开发 AspectJ](https://nmchgx.com/eclipse-idea/)
+ */
 public class HelloWorld {
     public static void main(String[] args) {
 
